f885e900885d17a0eac321cb43ecaf06d0612811
Lithesh Pulikkool <litheshp@gmail.com>
2024-06-28 22:01:46 +0530

    Updated dashboard with POB records per day

--- routes/v1/dashboard.js
+++ routes/v1/dashboard.js
@@ -1,17 +1,29 @@
 const Router = require('express').Router()
 
-const Well = require('../../model/well') 
+const Well = require('../../model/well')
 const User = require('../../model/user')
-const Employee =require('../../model/employee')
-const {authenticateToken}= require('../../utils/auth')
+const Employee = require('../../model/employee')
+const { authenticateToken } = require('../../utils/auth')
 Router.use(authenticateToken)
-Router.get('/',async (req,res)=>{
-   
+Router.get('/', async (req, res) => {
+
     const wellCount = await Well.countDocuments()
     const userCount = await User.countDocuments()
-    const POB =  await Employee.countDocuments({unit:req.user.unit})
+    // Get today's date at midnight
+    const startOfDay = new Date();
+    startOfDay.setHours(0, 0, 0, 0);
+
+    // Get the date for the end of today
+    const endOfDay = new Date();
+    endOfDay.setHours(23, 59, 59, 999);
+    const POB = await Employee.countDocuments({
+        unit: req.user.unit, POBDate: {
+            $gte: startOfDay,
+            $lte: endOfDay
+        }
+    })
     res.status(200).json({
-        wellCount,userCount,POB
+        wellCount, userCount, POB
     })
 })
 module.exports = Router
